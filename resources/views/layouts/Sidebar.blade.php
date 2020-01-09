ecaf5c55c17d7a7c8d318b37be7f7d9d79966301
root <wixancl@gmail.com>
2020-01-09 10:36:27 -0300

    nombre de usuario en sesion activa

--- resources/views/layouts/Sidebar.blade.php
+++ resources/views/layouts/Sidebar.blade.php
@@ -6,10 +6,8 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
-
-
       </div>
 
       @include("layouts.SidebarMenu")

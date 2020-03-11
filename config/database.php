b029ec0bc6e69bd52c61192ba600ef0d903fef82
“Carlos <carloantoniocc@gmail.com>
2020-03-11 04:08:49 +0000

    carga bd heroku

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL','pgsql://hffwtgxpxhzajh:4709c65ff56d38a2ace414cf9845bc13e9e3f28eb8edec2f73ec78a7d8160fe0@ec2-52-23-14-156.compute-1.amazonaws.com:5432/d2ujpnhid1p7t6'),
+            'host' => env('DB_HOST', 'ec2-52-23-14-156.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd2ujpnhid1p7t6'),
+            'username' => env('DB_USERNAME', 'hffwtgxpxhzajh'),
+            'password' => env('DB_PASSWORD', '4709c65ff56d38a2ace414cf9845bc13e9e3f28eb8edec2f73ec78a7d8160fe0'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,

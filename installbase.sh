34ba1c330e6926948db7a4228f9a486ed0450ce6
root <wixancl@gmail.com>
2021-05-09 03:39:02 +0000

    2021_05_09_03_39_01

--- installbase.sh
+++ installbase.sh
@@ -21,9 +21,32 @@ echo -e "\e[93m # wwww.wixan.cl                                 # \e[0m"
 echo -e "\e[93m #                                               # \e[0m"
 echo -e "\e[92m ################################################# \e[0m"
 echo -e "\e[92m  \e[0m"
-echo -e "\e[96m Escriba el Nombre del Controlador \e[0m"
+
+echo -e "\e[96m Escriba el Nombre o IP del servidor \e[0m"
 echo -n " >> "
-read nombre
+read  server
+echo  $server
+echo -e "\e[92m  \e[0m"
+
+echo -e "\e[96m Escriba el usuario de la base de datos \e[0m"
+echo -n " >> "
+read dbuser
+echo  $dbuser
+echo -e "\e[92m  \e[0m"
+
+
+echo -e "\e[96m Escriba la cotrasena del usaurio de la base de datos \e[0m"
+echo -n " >> "
+read dbuserpass
+echo  $dbuserpass
+echo -e "\e[92m  \e[0m"
+
+echo -e "\e[96m Escriba el nombre de la base de datos que desea crear \e[0m"
+echo -n " >> "
+read db
+echo  $db
+mysql -h $server -u $dbuser -p$dbuserpass -e "CREATE DATABASE $db;"
+
 
 
 
